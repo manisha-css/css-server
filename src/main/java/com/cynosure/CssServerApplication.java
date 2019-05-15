a88eebc2d328b6136dc526bcc7938dfaba5f976f
Manisha <manisha_msathe@yahoo.com>
2019-05-15 16:53:44 -0500

    Adding Spring Boot files

---
+++ src/main/java/com/cynosure/CssServerApplication.java
@@ -0,0 +1,13 @@
+package com.cynosure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssServerApplication.class, args);
+	}
+
+}
