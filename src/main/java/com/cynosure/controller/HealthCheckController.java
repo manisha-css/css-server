adf7dc4679aa1790d63e0d3df537970ec6f801f3
Manisha <manisha_msathe@yahoo.com>
2019-05-16 15:56:37 -0500

    Adding healthcheck related code + cors filter

---
+++ src/main/java/com/cynosure/controller/HealthCheckController.java
@@ -0,0 +1,23 @@
+package com.cynosure.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/healthcheck")
+public class HealthCheckController {
+
+  @RequestMapping(method = RequestMethod.GET)
+  public ResponseEntity<String> getHealthCheck(HttpServletRequest httpServletRequest) {
+
+    String result = "{\"result\":\"HealthOK\"}";
+
+    return new ResponseEntity<>(result, HttpStatus.OK);
+  }
+}
