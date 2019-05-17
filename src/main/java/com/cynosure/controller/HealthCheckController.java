96bd6f4388d796be6a37e8853d6f9a3cb3e6766d
Manisha <manisha_msathe@yahoo.com>
2019-05-17 17:13:39 -0500

    Added environment

--- src/main/java/com/cynosure/controller/HealthCheckController.java
+++ src/main/java/com/cynosure/controller/HealthCheckController.java
@@ -3,11 +3,9 @@ package com.cynosure.controller;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @RequestMapping("/healthcheck")
--- src/main/java/com/cynosure/filter/CorsFilter.java
+++ src/main/java/com/cynosure/filter/CorsFilter.java
@@ -7,20 +7,23 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CorsFilter implements Filter {
 
+  @Value("${whitelist.restclient.ip}")
+  String allowDomain;
+
   @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
       throws IOException, ServletException {
-      HttpServletResponse response = (HttpServletResponse) res;
-      response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-      response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-      response.setHeader("Access-Control-Max-Age", "3600");
-      response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
-      chain.doFilter(req, res);
-    }
-
+    HttpServletResponse response = (HttpServletResponse) res;
+    response.setHeader("Access-Control-Allow-Origin", allowDomain);
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+    response.setHeader("Access-Control-Max-Age", "3600");
+    response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
+    chain.doFilter(req, res);
+  }
 }
