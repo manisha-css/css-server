8e16548dbbe744597d7f54c8d4cc0e081537249e
Manisha <manisha_msathe@yahoo.com>
2019-05-17 21:03:14 -0500

    Logging added

--- src/main/java/com/cynosure/controller/HealthCheckController.java
+++ src/main/java/com/cynosure/controller/HealthCheckController.java
@@ -1,6 +1,8 @@
 package com.cynosure.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/healthcheck")
 public class HealthCheckController {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(HealthCheckController.class);
+
   @RequestMapping(method = RequestMethod.GET)
   public ResponseEntity<String> getHealthCheck(HttpServletRequest httpServletRequest) {
 
     String result = "{\"result\":\"HealthOK\"}";
+    LOGGER.debug("Debug log message");
+    LOGGER.info("Info log message");
+    LOGGER.error("Error log message");
+
 
     return new ResponseEntity<>(result, HttpStatus.OK);
   }
