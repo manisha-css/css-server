d0f87da3891560704eeba1e62b2bcc374b8d0bde
Manisha <manisha_msathe@yahoo.com>
2019-06-12 15:37:57 -0500

    Swagger related code

--- src/main/java/com/cynosure/CssServerApplication.java
+++ src/main/java/com/cynosure/CssServerApplication.java
@@ -2,11 +2,20 @@ package com.cynosure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class CssServerApplication {
+public class CssServerApplication implements WebMvcConfigurer {
+
+  private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {"classpath:/static/"};
 
   public static void main(String[] args) {
     SpringApplication.run(CssServerApplication.class, args);
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/html/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
   }
 }
