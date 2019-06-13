12a292f2b7fbab6cb003938b4422c800e38a4c80
Manisha <manisha_msathe@yahoo.com>
2019-06-13 17:18:29 -0500

    sonarqube translate related issues fixed

--- src/main/java/com/cynosure/controller/HealthCheckController.java
+++ src/main/java/com/cynosure/controller/HealthCheckController.java
@@ -6,24 +6,23 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/healthcheck")
 public class HealthCheckController {
-  @Autowired private MessageSource messageSource;
+  @Autowired private Translator translator;
 
   private static final Logger LOGGER = LoggerFactory.getLogger(HealthCheckController.class);
 
-  @RequestMapping(method = RequestMethod.GET)
+  @GetMapping
   public ResponseEntity<InfoResponse> getHealthCheck(HttpServletRequest httpServletRequest) {
     InfoResponse infoResponse = new InfoResponse();
-    infoResponse.setMessage(Translator.toLocale("healthcheck.ok", null));
+    infoResponse.setMessage(translator.toLocale("healthcheck.ok", null));
     LOGGER.debug("Debug log message");
     LOGGER.info("Info log message");
     LOGGER.error("Error log message");
--- src/main/java/com/cynosure/util/Translator.java
+++ src/main/java/com/cynosure/util/Translator.java
@@ -2,21 +2,16 @@ package com.cynosure.util;
 
 import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Translator {
 
-  private static ResourceBundleMessageSource messageSource;
+  @Autowired MessageSource messageSource;
 
-  @Autowired
-  Translator(ResourceBundleMessageSource messageSource) {
-    Translator.messageSource = messageSource;
-  }
-
-  public static String toLocale(String msgCode, Object[] params) {
+  public String toLocale(String msgCode, Object[] params) {
     Locale locale = LocaleContextHolder.getLocale();
     return messageSource.getMessage(msgCode, params, locale);
   }
